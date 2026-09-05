1f1b8239feab2c1734d3a459f4d893adf8572051
lakshyasree25-beep <lakshyasree25@gmail.com>
2026-09-04 20:40:19 -0500

    goal

--- hw/hw_9.py
+++ hw/hw_9.py
@@ -1,19 +1,17 @@
 import keyword
 
 # Get information
-name = input("Name: ")
-goal = input("Goal: ")
-month = input("Target month: ")
+name = input("Name?")
+goal = input("Goal?")
+month = input("Target month?")
 
 minutes = 30
 
 # Show goal plan
-print("\n--- PERSONAL GOAL ---")
-print("Name:", name)
-print("Goal:", goal)
-print("Month:", month)
-print("Practice:", minutes, "minutes a day")
+print("PERSONAL GOAL")
+print("name:",name)
+print("goal:",goal)
+print("Target month:", month)
+print("Practice:",minutes, "a day")
 
-print("\nStatus: Not Started")
-
-print("\n" + name, "will practice", goal, "for", minutes, "minutes each day.")
+print("Status: Not Started")
